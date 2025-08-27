f25997b43bb6e026e807963043a6b9a664ffbc97
Darkin <lordcigelsalvar@gmail.com>
2025-08-27 11:54:18 +0800

    Add user role enum and update User model and migration
    - Introduced new user roles in UserRole enum: CLUB_ADMIN, CLUB_MEMBER, CLUB_OWNER, CLUB_MODERATOR, CLUB_EDITOR, CLUB_VIEWER.
    - Added 'role' field to User model's fillable attributes.
    - Updated users migration to include 'role' field with a default value of 'user'.

--- app/Enums/UserRole.php
+++ app/Enums/UserRole.php
@@ -4,5 +4,12 @@ namespace App\Enums;
 
 enum UserRole
 {
+    case USER;
     case ADMIN;
+    case CLUB_ADMIN;
+    case CLUB_MEMBER;
+    case CLUB_OWNER;
+    case CLUB_MODERATOR;
+    case CLUB_EDITOR;
+    case CLUB_VIEWER;
 }
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('role')->default('user');
             $table->string('avatar')->nullable();
             $table->string('name');
             $table->string('email')->unique();
